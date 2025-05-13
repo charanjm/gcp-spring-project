5f24842d2ce87aeb6f9847fbf19255ce18aa3953
hari gadaputi <gaduputihari@gmail.com>
2025-05-13 11:02:44 +0530

    Initial Commit

---
+++ src/main/java/com/charan/GcpSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.charan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpSpringProjectApplication.class, args);
+	}
+
+}
